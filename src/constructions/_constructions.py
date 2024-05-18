3da9ff3b0189f9237b5a4504af3463525afd8906
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-05-18 22:16:58 +0100

    Add `plot` methods for `Point` and `Line`

--- src/constructions/_constructions.py
+++ src/constructions/_constructions.py
@@ -1,4 +1,6 @@
 import sympy as sp
+import numpy as np
+from jutility import plotting
 
 def dot(x, y):
     return x.T * y
@@ -13,12 +15,20 @@ class Point:
     def __init__(self, coords):
         self.coords = coords
 
+    def plot(self, **kwargs):
+        return plotting.Scatter(*self.coords, **kwargs)
+
 class Line:
     def __init__(self, a, b):
         self.a = a.coords
         self.b = b.coords
         self.bma = self.b - self.a
         self.bma_l2_sq = l2_sq(self.bma)
+
+    def plot(self, **kwargs):
+        a = np.array(self.a).flatten().astype(float)
+        b = np.array(self.b).flatten().astype(float)
+        return plotting.AxLine(a, b, **kwargs)
 
 class Circle:
     def __init__(self, centre, r_sq):
