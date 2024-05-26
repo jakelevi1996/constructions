b64861d6bd6efb166902e55ad7937ca0a6292bf6
jakelevi1996 <jakelevi@hotmail.co.uk>
2024-05-26 16:20:57 +0100

    Add `random_rational` and `random_point` functions and refactor unit test to use them

--- tests/test_constructions.py
+++ tests/test_constructions.py
@@ -1,4 +1,5 @@
 import os
+import math
 import sympy as sp
 import numpy as np
 from jutility import plotting, util
@@ -9,27 +10,36 @@ RESULTS_DIR = os.path.join(CURRENT_DIR, "Outputs")
 
 util.numpy_set_print_options()
 
+def random_rational(rng, lo=-1, hi=1, denom_lo=50, denom_hi=100):
+    denom = rng.integers(denom_lo, denom_hi)
+    num_lo = math.ceil( denom * lo)
+    num_hi = math.floor(denom * hi)
+    num = rng.integers(num_lo, num_hi)
+    rational = sp.Rational(num, denom)
+    assert (lo <= rational) and (rational <= hi)
+    return rational
+
+def random_point(*args, **kwargs):
+    x = random_rational(*args, **kwargs)
+    y = random_rational(*args, **kwargs)
+    m = sp.Matrix([x, y])
+    p = cn.Point(m)
+    return p
+
 def test_plot():
     rng = util.Seeder().get_rng("test_plot")
-    a, b, c, d = [rng.integers(-10, 10, 2) for _ in range(4)]
-
-    a = cn.Point(sp.Matrix(a))
-    b = cn.Point(sp.Matrix(b))
-    c = cn.Point(sp.Matrix(c))
-    d = cn.Point(sp.Matrix(d))
-    ab_line = cn.Line(a, b)
-    cd_line = cn.Line(c, d)
+    a, b, c, d = [random_point(rng) for _ in range(4)]
 
     plotting.plot(
         *[p.plot(c="r", s=100, zorder=20) for p in [a, b, c, d]],
-        *[line.plot(c="b") for line in [ab_line, cd_line]],
+        *[s.plot(c="b") for s in [cn.Line(a, b), cn.Line(c, d)]],
         *[
             cn.Circle(a, a.l2_sq_distance(x)).plot(c="m")
             for x in [b, c, d]
         ],
         figsize=[8, 6],
-        xlim=[-20, 20],
-        ylim=[-20, 20],
+        ylim=[-2, 2],
+        xlim=[-2, 2],
         axis_equal=True,
         plot_name="test_plot",
         dir_name=RESULTS_DIR,
